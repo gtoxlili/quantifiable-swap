5c376b1df04b78f32335f78b668ce16c7a268d54
gtoxlili <664037691@qq.com>
2025-02-18 18:41:25 +0800

    refactor: improve error handling in main function and extract application logic

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gtoxlili/quantifiable-swap/common/config"
 	"github.com/gtoxlili/quantifiable-swap/common/job"
 	"github.com/gtoxlili/quantifiable-swap/common/logger"
+	"github.com/rs/zerolog"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,16 +15,23 @@ import (
 func main() {
 	log := logger.NewGeneralLogger()
 
-	log.Info().Msg("应用程序启动中...")
-	jobs, err := config.ParseConfig("./config.yaml")
-	if err != nil {
+	if err := runApp(log, "./config.yaml"); err != nil {
 		log.Panic().
 			Err(err).
-			Msg("配置文件解析失败")
+			Msg("Application exited with error")
+	}
+}
+
+func runApp(log zerolog.Logger, configPath string) error {
+	log.Info().Msg("应用程序启动中...")
+	jobs, err := config.ParseConfig(configPath)
+	if err != nil {
+		return fmt.Errorf("配置文件解析失败：%w", err)
 	}
 
 	manager := job.NewManager()
 	log.Info().Int("任务数量", len(jobs)).Msg("开始调度任务")
+
 	for _, j := range jobs {
 		jobConfig, _ := json.MarshalIndent(j, "", "  ")
 		id, err := manager.AddJob(j)
@@ -43,8 +52,9 @@ func main() {
 	signChan := make(chan os.Signal, 1)
 	signal.Notify(signChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	sig := <-signChan
+
 	// 保存任务到配置文件
-	if err := config.SaveConfig("./config.yaml", manager.JobsData()); err != nil {
+	if err := config.SaveConfig(configPath, manager.JobsData()); err != nil {
 		log.Error().
 			Err(err).
 			Msg("保存配置文件失败")
@@ -53,4 +63,6 @@ func main() {
 	log.Info().
 		Str("Sign", sig.String()).
 		Msg("接收到终止信号，程序关闭")
+
+	return nil
 }
