e7a952e51545e77449dd57466c037bc569a9fbcf
gtoxlili <664037691@qq.com>
2025-02-19 14:33:00 +0800

    refactor: restructure application to encapsulate logging and job management

--- main.go
+++ main.go
@@ -3,66 +3,107 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	tgApi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/gtoxlili/quantifiable-swap/bot"
+	"github.com/gtoxlili/quantifiable-swap/bot/handler"
 	"github.com/gtoxlili/quantifiable-swap/common/config"
 	"github.com/gtoxlili/quantifiable-swap/common/job"
 	"github.com/gtoxlili/quantifiable-swap/common/logger"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
-func main() {
-	log := logger.NewGeneralLogger()
+type App struct {
+	log        zerolog.Logger
+	configPath string
+	manager    job.IManager
+	bot        *tgApi.BotAPI
+}
 
-	if err := runApp(log, "./config.yaml"); err != nil {
-		log.Panic().
-			Err(err).
-			Msg("Application exited with error")
+func NewApp(configPath string) *App {
+	return &App{
+		log:        logger.NewGeneralLogger(),
+		configPath: configPath,
+		manager:    job.NewManager(),
+		bot:        bot.Bot,
 	}
 }
 
-func runApp(log zerolog.Logger, configPath string) error {
-	log.Info().Msg("应用程序启动中...")
-	jobs, err := config.ParseConfig(configPath)
+func (app *App) initialize() error {
+	app.log.Info().Msg("应用程序初始化中...")
+	jobs, err := config.ParseConfig(app.configPath)
 	if err != nil {
-		return fmt.Errorf("配置文件解析失败：%w", err)
+		return fmt.Errorf("配置文件解析失败: %w", err)
 	}
 
-	manager := job.NewManager()
-	log.Info().Int("任务数量", len(jobs)).Msg("开始调度任务")
+	app.log.Info().Int("任务数量", len(jobs)).Msg("开始调度任务")
+	app.initJobs(jobs)
 
+	if app.bot != nil {
+		app.initBot()
+	}
+	return nil
+}
+
+func (app *App) initJobs(jobs []config.Job) {
 	for _, j := range jobs {
 		jobConfig, _ := json.MarshalIndent(j, "", "  ")
-		id, err := manager.AddJob(j)
+		id, err := app.manager.AddJob(j)
 		if err != nil {
-			log.Error().
+			app.log.Error().
 				Err(err).
 				Bytes("任务配置", jobConfig).
 				Msg("任务启动失败")
 			continue
 		}
-		_ = manager.RunJob(id)
-		log.Info().
+		_ = app.manager.RunJob(id)
+		app.log.Info().
 			Bytes("任务配置", jobConfig).
 			Msg("任务启动成功")
 	}
+}
 
-	log.Info().Msg("所有任务已启动，等待终止信号...")
+func (app *App) initBot() {
+	app.log.Info().Msg("启动 Telegram 机器人")
+	go handler.NewBotHandler(app.bot, app.manager).StartDispatching()
+}
+
+func (app *App) cleanup(sig os.Signal) {
+	if err := config.SaveConfig(app.configPath, app.manager.JobsData()); err != nil {
+		app.log.Error().Err(err).Msg("保存配置文件失败")
+	}
+	if app.bot != nil {
+		app.bot.MakeRequest("deleteWebhook", tgApi.Params{})
+		app.bot.StopReceivingUpdates()
+	}
+	app.manager.RemoveAll()
+
+	log.Info().
+		Str("Sign", sig.String()).
+		Msg("接收到终止信号，程序关闭")
+}
+
+func (app *App) run() error {
+	if err := app.initialize(); err != nil {
+		return err
+	}
+
+	app.log.Info().Msg("所有服务已启动，等待终止信号...")
+
 	signChan := make(chan os.Signal, 1)
 	signal.Notify(signChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	sig := <-signChan
 
-	// 保存任务到配置文件
-	if err := config.SaveConfig(configPath, manager.JobsData()); err != nil {
-		log.Error().
-			Err(err).
-			Msg("保存配置文件失败")
-	}
-	manager.RemoveAll()
-	log.Info().
-		Str("Sign", sig.String()).
-		Msg("接收到终止信号，程序关闭")
-
+	app.cleanup(sig)
 	return nil
+}
+
+func main() {
+	app := NewApp("./config.yaml")
+	if err := app.run(); err != nil {
+		app.log.Panic().Err(err).Msg("应用程序异常退出")
+	}
 }
